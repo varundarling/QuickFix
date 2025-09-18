ebcba310e744ab976fa2a49bb8e5ac0a45a7260a
Varun <varundarling7165@gmail.com>
2025-09-18 19:23:29 +0530

    Fixed some build issues

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,10 @@
 pluginManagement {
-
     val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
+        val sdkPath = properties.getProperty("flutter.sdk")
+        require(sdkPath != null) { "flutter.sdk not set in local.properties" }
+        sdkPath
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -18,8 +17,8 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"    // do NOT use apply false
+    id("com.android.application") version "8.7.3" apply false  // keep actual AGP version
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
